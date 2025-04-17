49709d6500b602c17a531a4e881f42ccf4dea08d
Empester <secur2008treb@gmail.com>
2025-04-17 06:06:20 +0300

    test

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,7 +3,7 @@ import { join } from "https://deno.land/std@0.144.0/path/mod.ts";
 
 export const environment = {
   // Backend from where the information is fetched
-  baseUrl: "https://www.apicountries.com/alpha/",
+  baseUrl: "https://www.apicountries.com/",
   // Update cache if it has been longer than given DAYS since last sync
   syncInterval: 7,
   // Directory where country information should be stored.
@@ -11,6 +11,5 @@ export const environment = {
   // Determines the size of ASCII art flag size
   flagWidth: 40,
   // Fields that should be fetched from API
-  queries:
-    "all?fields=name,capital,currencies,population,flag,languages,region,subregion,timezones,latlng,capitalInfo,tld,flags",
+  queries: "all?fields=name,capital,currencies,population,flags,languages,region,subregion,timezones,latlng",
 };
