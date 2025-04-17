66070a8034ab332ec136e9315c4a5192530e058a
Empester <secur2008treb@gmail.com>
2025-04-17 06:09:39 +0300

    test

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,15 +1,21 @@
 import home_dir from "https://deno.land/x/dir@1.4.0/home_dir/mod.ts";
 import { join } from "https://deno.land/std@0.144.0/path/mod.ts";
 
+const home = home_dir();
+if (!home) {
+  throw new Error("Unable to determine home directory.");
+}
+
 export const environment = {
   // Backend from where the information is fetched
-  baseUrl: "https://www.apicountries.com/",
+  baseUrl: "https://restcountries.com/v3.1/",
   // Update cache if it has been longer than given DAYS since last sync
   syncInterval: 7,
-  // Directory where country information should be stored.
-  cacheDir: join(home_dir() as string, ".cache", "countryfetch"),
+  // Directory where country information should be stored
+  cacheDir: join(home, ".cache", "countryfetch"),
   // Determines the size of ASCII art flag size
   flagWidth: 40,
   // Fields that should be fetched from API
-  queries: "all?fields=name,capital,currencies,population,flags,languages,region,subregion,timezones,latlng",
+  queries:
+    "all?fields=name,capital,currencies,population,flags,languages,region,subregion,timezones,latlng",
 };
