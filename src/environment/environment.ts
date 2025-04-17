d03ea3e2b910577a4ad20fe8802107718e0cb9b5
Empester <secur2008treb@gmail.com>
2025-04-17 06:00:37 +0300

    api test

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,7 +3,7 @@ import { join } from "https://deno.land/std@0.144.0/path/mod.ts";
 
 export const environment = {
   // Backend from where the information is fetched
-  baseUrl: "https://restcountries.com/v3.1/",
+  baseUrl: "https://www.apicountries.com/alpha/",
   // Update cache if it has been longer than given DAYS since last sync
   syncInterval: 7,
   // Directory where country information should be stored.
